026ae8ac93e656cefb534478e62255c9d918a183
mahbubur.rahman <mahbubur.rahman@bjitgroup.com>
2019-08-19 14:27:29 +0900

    Added argument parser to pass argument to run the code

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 ## Yoon kim paper implementation
+
+<p> Implemented static and non static cnn for kim yoon paper </p>
+
+### Pre requisites:
+ - Keras
+ - Python 3
+
+### How to run/train model?
+
+ - Run by using :
+    
+   - python3 main.py [model_name]
+
+<p> Here model_name can be static or non_static </p>
+ 
--- main.py
+++ main.py
@@ -10,6 +10,7 @@ from data_load_and_preproccess import data_load_and_preproccess
 from word_to_vector_pretrained import word_to_vector
 import numpy as np
 from models import Models
+
 model_properties = {
         "word_size": 5000,
         "sequence_length": 500,
@@ -24,26 +25,16 @@ model_properties = {
         "epochs" : 10,
         }
 
-model = Models(model_properties)
-model.cnn_non_static()
-
-#
-#if(len(sys.argv)==2):
-#    action = sys.argv[1]
-#    model = Model(model_properties)
-#    if action == 'CNN-static':
-#        model.cnn_static()
-#    else:
-#        print('lol')
-#        
-#        
-#        
-#        
-#    
-#        
-#else:
-#    print('please run like this : python main.py [model_name]')
-#    print('please select model_name among [CNN-rand, CNN-static, CNN-non-static ]')
+if(len(sys.argv)==2):
+    action = sys.argv[1]
+    model = Model(model_properties)
+    if action == 'CNN-static':
+        model.cnn_static()
+    else:
+        model.cnn_non_static()
+else:
+    print('please run like this : python main.py [model_name]')
+    print('please select model_name among [CNN-rand, CNN-static, CNN-non-static ]')
     
 
 
